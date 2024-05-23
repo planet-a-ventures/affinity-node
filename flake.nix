b65d75c104e53286c519fac9cadd9ec3fad8deec
Joscha Feth <joscha@feth.com>
2024-05-23 14:21:34 +0100

    chore: flake

--- flake.nix
+++ flake.nix
@@ -8,17 +8,17 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        inherit (pkgs) mkShell;
-      in {
-        overlay = final: prev: { };
-
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ deno nodejs-slim_20 esbuild nixfmt ];
-        };
-      });
-
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      inherit (pkgs) mkShell;
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [ deno nodejs-slim_20 esbuild nixfmt ];
+      };
+    }) // {
+    overlays = {
+      default = final: prev: { };
+    };
+  };
 }
