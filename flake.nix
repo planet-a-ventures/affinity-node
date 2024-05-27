0067d0f520eb46f668ffaa55300a99c4e5d89018
Joscha Feth <joscha@feth.com>
2024-05-27 20:29:18 +0100

    chore: git hooks via nix

--- flake.nix
+++ flake.nix
@@ -4,39 +4,54 @@
   nixConfig.bash-prompt = "[ts-deno-node]> ";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        inherit (pkgs) mkShell;
-      in
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake
+      { inherit inputs; }
       {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            deno
-            nodejs-slim_20
-            esbuild
-            nixfmt-rfc-style
-            # For openapi-generator
-            # If more dependencies are needed, investigate whether to load https://github.com/OpenAPITools/openapi-generator/blob/master/flake.nix
-            jdk11
-          ];
+        imports = [
+          inputs.pre-commit-hooks-nix.flakeModule
+        ];
+        systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+        perSystem = { config, self', inputs', pkgs, ... }: {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+
+          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+          # packages.hello = pkgs.hello;
+          pre-commit.settings.hooks.nixpkgs-fmt.enable = true;
+          pre-commit.settings.hooks.commitizen.enable = true;
+          pre-commit.settings.hooks.denofmt.enable = true;
+          pre-commit.settings.hooks.denolint.enable = true;
+
+          # NOTE: You can also use `config.pre-commit.devShell`
+          devShells.default = pkgs.mkShell {
+            shellHook = ''
+              ${config.pre-commit.installationScript}
+              echo 1>&2 "Welcome to the development shell!"
+            '';
+            buildInputs = with pkgs; [
+              deno
+              nodejs-slim_20
+              esbuild
+              nixfmt-rfc-style
+              # For openapi-generator
+              # If more dependencies are needed, investigate whether to load https://github.com/OpenAPITools/openapi-generator/blob/master/flake.nix
+              jdk11
+            ];
+          };
         };
-      }
-    )
-    // {
-      overlays = {
-        default = final: prev: { };
+        flake = {
+          # The usual flake attributes can be defined here, including system-
+          # agnostic ones like nixosModule and system-enumerating ones, although
+          # those are more easily expressed in perSystem.
+
+        };
       };
-    };
 }
