51c78efcebc710b17c4b468fcaa7bd59ff34f1d3
Joscha Feth <joscha@feth.com>
2024-05-28 17:50:51 +0100

    docs: add TODO for dates-based tests

--- src/v1/tests/fields_test.ts
+++ src/v1/tests/fields_test.ts
@@ -24,6 +24,8 @@ describe('fields', () => {
         mock?.reset()
     })
 
+    // TODO(@joscha): Add tests for dates
+
     describe('all', () => {
         it('fetches all fields', async (t) => {
             const fixture = await getRawFixture('fields/all.raw.response.json')
