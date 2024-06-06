e36ee9986bc9938f5e1d3ba5fd8aa76bdbe71859
Joscha Feth <joscha@feth.com>
2024-06-07 00:32:41 +0100

    fix: organization search & error handling (#9)

--- src/v1/axios_default_transformers.ts
+++ src/v1/axios_default_transformers.ts
@@ -1,8 +1,16 @@
 import axios, { AxiosResponseTransformer } from 'axios'
+import { AffinityApiError } from './errors.ts'
+
+const errorTransformer: AxiosResponseTransformer = (data, headers, status) => {
+    if (status && (status < 200 || status >= 300)) {
+        throw new AffinityApiError(status, data)
+    }
+    return data
+}
 
 export const defaultTransformers = (): AxiosResponseTransformer[] => {
     const { transformResponse = [] } = axios.defaults
     return Array.isArray(transformResponse)
-        ? transformResponse
-        : [transformResponse]
+        ? [...transformResponse, errorTransformer]
+        : [transformResponse, errorTransformer]
 }
